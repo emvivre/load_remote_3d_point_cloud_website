2d2c76eea2f4b7b293dfd2c09bb8a1764d2f9f61
emvivre <emvivre@urdn.com.ua>
2019-03-10 14:12:57 +0100

    * add project code

---
+++ app/src/main/java/com/flozes/webpage/MainActivity.java
@@ -0,0 +1,39 @@
+package com.flozes.webpage;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private static final String URL_TO_LOAD = "https://lozes.users.greyc.fr";
+
+    private WebView mWebView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView = findViewById(R.id.activity_main_webview);
+        mWebView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setSupportZoom(true);
+        webSettings.setBuiltInZoomControls(true);
+        mWebView.loadUrl( URL_TO_LOAD );
+        mWebView.setWebViewClient(new MyWebViewClient());
+    }
+
+    // Prevent the back-button from closing the app
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+}
